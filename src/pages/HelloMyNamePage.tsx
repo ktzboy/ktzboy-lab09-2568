d69f8061c452534363d67dd4b5a38f6f290b4ba0
ktzboy <pongpol.sritalanut@gmail.com>
2025-08-05 20:42:30 +0700

    done

--- src/pages/HelloMyNamePage.tsx
+++ src/pages/HelloMyNamePage.tsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
 export default function HelloMyNamePage() {
+  const [name, setName] = useState("");
+  const ipname = (event : any) => {
+    setName(event.target.value);
+  }
+
+  const [result, setResult] = useState("Please insert your name")
+  const [isgreet, setIsGreet] = useState(false);
+  const greetonclick = () => {
+    if(name === ""){
+      setResult("Please insert your name");
+      setIsGreet(false);
+    } else{
+      setResult(`Hello ${name}`);
+      setIsGreet(true);
+    }
+  }
+
   return (
     <div className="container text-center">
       <h3>Hello My Name</h3>
@@ -6,10 +24,12 @@ export default function HelloMyNamePage() {
         className="form-control d-inline m-1"
         placeholder="Insert your name here!"
         style={{ width: "300px" }}
+        onChange={ipname}
+        value={name}
       />
-      <button className="btn btn-primary">Greet Me</button>
+      <button className="btn btn-primary" onClick={greetonclick}>Greet Me</button>
       {/* Result Text */}
-      <p></p>
+      <p className={isgreet ? "" : "text-danger"}>{result}</p>
     </div>
   );
 }
--- src/pages/TodolistPage.tsx
+++ src/pages/TodolistPage.tsx
@@ -45,11 +45,13 @@ function App() {
     setTasks(newTasks);
   };
 
+  const nclick = tasks.filter((task) => task.isDone).length;
+
   return (
     <div className="col-12 m-2 p-0">
       <div className="container text-center">
         <h2>Todo List</h2>
-        <span className="m-2">All : () Done : ()</span>
+        <span className="m-2">All : ({tasks.length}) Done : ({nclick})</span>
         {/* Modal Component */}
         <button
           type="button"
